4b45319759286587dce32e5443c44b09286dae70
Laetys <laetys33@gmail.com>
2024-01-13 21:36:20 +0100

    Start rope

--- game/scene/entitites/corde.gd
+++ game/scene/entitites/corde.gd
@@ -8,11 +8,15 @@ var list_joint = []
 var number_joints = 0
 var dist_overlap_joint = 0
 
+var length=0
+
+func _move_extremities():
+	pass
 func add_piece():
 	pass
 func remove_piece():
 	pass
-func generatore_rope():
+func generate_rope(nb_pieces):
 	pass
 
 
