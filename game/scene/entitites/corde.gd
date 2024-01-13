d0fbe7ceadb515063af5004888277f8cc1991c10
Laetys <laetys33@gmail.com>
2024-01-13 21:31:28 +0100

    rope

---
+++ game/scene/entitites/corde.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+var extremity1
+var extremity2
+var list_pieces = []
+@export var number_pieces = 0
+var list_joint = []
+var number_joints = 0
+var dist_overlap_joint = 0
+
+func add_piece():
+	pass
+func remove_piece():
+	pass
+func generatore_rope():
+	pass
+
+
