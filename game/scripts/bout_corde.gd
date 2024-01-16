f53b6d96223d127767334d97aa59977587c1bb2d
Romain DELPY <romain.delpy@u-bordeaux.fr>
2024-01-16 19:18:13 +0100

    rope generate to all length

--- game/scripts/bout_corde.gd
+++ game/scripts/bout_corde.gd
@@ -4,12 +4,7 @@ extends RigidBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print_debug("rope ",id," ready!")
-	#if id %2 != 0:
-		#get_node("Sprite2D").texture = load("res://sprite/rope.png")
-	#else:
-		#get_node("Sprite2D").texture = load("res://sprite/rope_white.png")
-	 # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- game/scripts/corde.gd
+++ game/scripts/corde.gd
@@ -5,22 +5,30 @@ var Math = preload("res://scripts/Math.gd").new()
 
 var Piece = preload("res://scene/entitites/bout_corde.tscn")
 
-@export var distance=0
+## distance between the two extremities
+@export var distance=0 
 
+## the two extremitites of the rope
+@export var extremity1:Node2D
+@export var extremity2:Node2D
 
-var extremity1
-var extremity2
+## The pieces of the rope
 var list_pieces:Array[RigidBody2D]= []
+@export var nb_pieces:int= 0 # Will be computed from the distance at rope generation
 
-@export var nb_pieces:int= 0
 var list_joint:Array[PinJoint2D] = []
 var nb_joints:int = 0
+
+## Number of pixel the pieces will overlap each other (to connect the joint)
 @export var dist_overlap_joint = 2
+
+## Compute the distance between the joints in the rope
+var joint_distance:int
 
 
 
 	
-#Add a joint between obj1 and obj2 at pos_in_obj1 in obj1, and set the joint as child of obj1
+## Add a joint between obj1 and obj2 at pos_in_obj1 in obj1, and set the joint as child of obj1
 func _add_joint(joint,obj1,obj2,pos_in_obj1):
 	joint.node_a = obj1.get_path()
 	obj1.add_child(joint)
@@ -30,23 +38,29 @@ func _add_joint(joint,obj1,obj2,pos_in_obj1):
 	joint.bias = 0.9
 	
 
+## Compute the position of the piece corresponding to the optimal high tension rope
 func _compute_piece_position(piece,id,direction):
-	return direction*(0.5*piece.length - dist_overlap_joint + id * (piece.length - dist_overlap_joint))
+	return direction*(0.5*piece.length - dist_overlap_joint + id * (joint_distance))
 
 
 func _move_extremities(length):
 	var directionE1 = (extremity2.global_position-extremity1.global_position).normalized()
 	var directionE2 = -directionE1 
-	extremity1.get_parent().global_position += directionE1*(length/2)
+	extremity1.get_parent().global_position += directionE1*(length/2) # TODO : Need to treat this more clean
 	extremity2.global_position += directionE2*(length/2)
 	
 	
+## Compute the angle in the isosceles rectangle between base and
+## edges, where base is distance_to_joint and edges are joint_distance_in_piece
+func _angle_overlap(joint_distance_in_piece,distance_to_joint):
+	return acos((distance_to_joint**2)/(2.0*(joint_distance_in_piece)*distance_to_joint))
+
 
 func add_piece():
+	## General creation of a piece
 	var piece = Piece.instantiate()
 	add_child(piece)
 	piece.id = len(list_pieces)
-	nb_pieces+=1
 	list_pieces.append(piece)
 	
 	
@@ -56,77 +70,96 @@ func add_piece():
 	
 	
 	var direction = (extremity2.global_position-extremity1.global_position).normalized()
-	print("direction:",direction)
+	
 	
 	
 	var vectorPiece = Vector2(0,piece.length)
 	var rotationPoint = extremity1.global_position
 	var angleRotation = Math.angleBtVect(vectorPiece,direction)
-	print(angleRotation)
+	
+	## Case of the first piece
 	if (piece.id==0): 
 		var piecePostion = rotationPoint + _compute_piece_position(piece,piece.id,direction)
 		#var coordRotation = Math.new_coord_after_rotation(piecePostion,rotationPoint,-angleRotation)
-		#print(piecePostion, coordRotation)
 		piece.global_position= piecePostion
 		
 		piece.rotation -= (angleRotation)
 		
 		_add_joint(joint,extremity1,piece,Vector2.ZERO)
 		
-	elif (len(list_pieces)==nb_pieces):
-		pass #TODO: faire le code quand dernière pièce
+	## General case
 	else:
+		# General computation
 		var joint_pos_in_piece = Vector2(0,0.5*(piece.length-2*dist_overlap_joint)+ 0.5*dist_overlap_joint)
-		
-		print("distance:",distance)
-		var rope_length = (piece.id+1)*(piece.length-dist_overlap_joint)+0.5*dist_overlap_joint
-		print("length:",rope_length)
+		var rope_length = (piece.id+1)*(joint_distance)+0.5*dist_overlap_joint
+
+	## We have an overlap of rope length, need to angle the last piece and joint with the extremity2
 		if(rope_length>distance):
 			var piece_id_m_1 = list_pieces[piece.id-1]
 			var ancestor_joint_pos = list_joint[piece.id-1].global_position # we need this joint as
 			# a pivot point for the piece id-1
+			
+			# Compute the distance to cover and the new angle of the pieces
 			var dist_ancest_extrem = ancestor_joint_pos.distance_to(extremity2.global_position)
-			var angle_overlap = acos((dist_ancest_extrem**2)/(2.0*(piece.length-dist_overlap_joint)*dist_ancest_extrem))
+			var angle_overlap =  _angle_overlap(joint_distance,dist_ancest_extrem)
+			
+			# Making pivot the previous rope point
 			var new_pos_id_m_1 = Math.new_coord_after_rotation(
 				piece_id_m_1.global_position,ancestor_joint_pos,angle_overlap)
 			piece_id_m_1.global_position = new_pos_id_m_1
 			piece_id_m_1.rotation += angle_overlap
-			var new_piece_position = extremity2.global_position -direction*0.5*(piece.length - dist_overlap_joint)
+			
+			# Making pivot the new rope point
+			var new_piece_position = extremity2.global_position -direction*0.5*(joint_distance)
 			new_piece_position = Math.new_coord_after_rotation(
 				new_piece_position,extremity2.global_position,-angle_overlap)
-			#print(angle_overlap)
 			piece.global_position = new_piece_position
 			piece.rotation -= angleRotation + angle_overlap
-			#piece.rotation += 2*angle_overlap
 
+	## General case, add after the previous one
 		else:
 			var piecePostion = rotationPoint + _compute_piece_position(piece,piece.id,direction)
-			
 			piece.global_position = piecePostion
-			
 			piece.rotation -= angleRotation
+		
 		_add_joint(joint,list_pieces[piece.id-1],piece,joint_pos_in_piece)
-		if(rope_length>distance):
+		
+		if(rope_length>distance && list_joint[nb_joints - 1].node_b != extremity2.get_path()):
+			# Need to joint to the extremity
 			var joint_end = PinJoint2D.new()
 			nb_joints+=1
 			list_joint.append(joint_end)
 			_add_joint(joint_end,piece,extremity2,joint_pos_in_piece)
-		
+	# TODO: treat the case where the rope is added in the middle (i.e. extremities are connected)
 		
 func remove_piece():
 	pass
+
 func generate_rope(nb_pieces):
 	for i in range(nb_pieces):
 		add_piece()
 
 func _ready():
-	extremity2 = get_parent().get_node("AnchorPoint")
-	extremity1 = get_parent().get_node("CharacterBody2D").get_node("AnchorPoint")
-	distance = extremity1.global_position.distance_to(extremity2.global_position)
-	print_debug(nb_pieces)
-	generate_rope(nb_pieces)
+	#extremity2 = get_parent().get_node("AnchorPoint")
+	#extremity1 = get_parent().get_node("CharacterBody2D").get_node("AnchorPoint")
 	
-	pass # Replace with function body.
+	distance = 0
+	nb_pieces = 0
+	nb_joints = 0
+	
+	# Need to instantiate a piece of rope to know the length
+	var piece_loc = Piece.instantiate()
+	joint_distance = piece_loc.length -dist_overlap_joint
+	piece_loc.free()
+	
+
+func _input(event):
+	if(Input.is_action_just_pressed("general_event")):
+		# Computing the length of the rope
+		distance = extremity1.global_position.distance_to(extremity2.global_position)
+		nb_pieces = ceil(distance/joint_distance)
+		print_debug("dist: ",distance,"\nspace between joint: ", joint_distance,"\n nb pieces: ",nb_pieces)
+		generate_rope(nb_pieces)
 
 func _physics_process(delta):
 	pass
